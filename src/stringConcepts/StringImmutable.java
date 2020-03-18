bf519a47c56d919254efe08437592d110e59b3e9
RaghavSeth1 <raghav.seth001@gmail.com>
2020-03-18 07:41:48 +0530

    Commit

--- src/stringConcepts/StringImmutable.java
+++ src/stringConcepts/StringImmutable.java
@@ -3,6 +3,7 @@ package stringConcepts;
 public  class StringImmutable {
 	static String s1= "College";
 	static String s2=new String("College");
+	s1.
 
 	public static void main(String[] args) {
 		System.out.println(s1+" "+s2);
